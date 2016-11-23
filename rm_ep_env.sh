1fc03c88015638cca91bf90653e25b9d83c4ca29
Vy Nguyen <oontvoo@hotmail.com>
2016-11-23 13:30:12 -0500

    add rm_ep_env

---
+++ rm_ep_env.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+rm -rf 'sb-server-tests/derby.log'
+rm -rf 'sb-server-tests/pstack.application::org_eclipse_jdt_internal_junit_runner_RemoteTestRunner0.55747'
+rm -rf 'sb-server/nested.resources.nested-resource-path-resolver-test.txt'
+rm -rf 'sb-server/resource-path-resolver-test.txt'

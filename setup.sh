b7d88173f1c9705ad47201d91a4b951587b837db
Vy Nguyen <oontvoo@hotmail.com>
2016-12-01 13:13:42 -0500

    include for stuff for rm_ep_env

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # java and maven
 export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home'
-export M2_HOME='Maven home: /Users/vynguye/Downloads/apache-maven-3.3.3'
+export M2_HOME='/Users/vynguye/Downloads/apache-maven-3.3.3'
 export PATH=$JAVA_HOME/bin:$M2_HOME/bin:$PATH
 
 # tibco sb

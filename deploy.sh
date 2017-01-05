3b7f5e776b35181a373c8b2c2348073395ded19b
Vy Nguyen <oontvoo@hotmail.com>
2017-01-05 14:42:55 -0500

    clean up accidental trash

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -jar ${TIBCO_EP_HOME}/distrib/kabira/java/deploy.jar servicename=A.vynguye $1
-Vys-Macintosh:eventflow vynguye$ echo $SW_HOME
+java  -jar ${TIBCO_EP_HOME}/distrib/kabira/java/deploy.jar servicename=A.vynguye $1
+

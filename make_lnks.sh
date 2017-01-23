ef098e007438065f7ebecea8ede5cf07dc310ade
Vy Nguyen <oontvoo@hotmail.com>
2017-01-23 12:06:13 -0500

    need maven-lib linked, too

--- make_lnks.sh
+++ make_lnks.sh
@@ -3,3 +3,10 @@ ln -s /opt/tibco/users/ephome/3rdparty .
 
 mkdir -p sdk/maven/repo/
 ln -s /opt/tibco/users/ephome/BUILD/repository/* sdk/maven/repo/.
+
+rm compiler/maven-lib/server-10.1.0-SNAPSHOT.jar
+rm compiler/maven-lib/server-10.1.0-SNAPSHOT-sources.jar
+
+ln -s /opt/tibco/users/ephome/BUILD/repository/com/tibco/ep/sb/server/10.1.0-SNAPSHOT/server-10.1.0-SNAPSHOT.jar compiler/maven-lib/.
+
+ln -s /opt/tibco/users/ephome/BUILD/repository/com/tibco/ep/sb/server/10.1.0-SNAPSHOT/server-10.1.0-SNAPSHOT-sources.jar compiler/maven-lib/.

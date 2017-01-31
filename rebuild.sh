d0b06a2bbc6cab85a43832d290902db356e48071
Vy Nguyen <oontvoo@hotmail.com>
2017-01-31 13:47:46 -0500

    add rebuild

---
+++ rebuild.sh
@@ -0,0 +1,16 @@
+cd sbrt
+sw_mvn clean --fail-never
+cd ..
+cd dtm
+sw_mvn clean --fail-never
+cd ..
+rm -rf ${TIBCO_EP_HOME}/distrib
+rm -rf ${TIBCO_EP_HOME}/3rdparty
+rm -rf ${TIBCO_EP_HOME}/dependency-maven-plugin-markers
+for codeline in parent ktools epmaven dtm dtmfilestore sbrt rms dtmchannel dtmbusinessstatemachine dtmexamples
+do
+    cd ${codeline}
+    git pull
+    time sw_mvn -p 8 -d -u
+    cd ..
+done
